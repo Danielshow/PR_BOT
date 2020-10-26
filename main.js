a0ef63b8028f9c102dc49ac43d312dc00e3d393b
Danielshow <Danielshotonwa53@gmail.com>
2020-10-26 16:44:53 +0100

    Set up server 🤯

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 // ESM syntax is supported.
-export {}
+import express from 'express';
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+const PORT = process.env.PORT || 9400;
+
+app.listen(PORT, () => {
+  console.log(`PR slack bot started on port ${PORT}`);
+});
